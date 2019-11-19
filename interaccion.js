10fe253bcceafa57e9699462970285efaba7a9e6
Seba Carrasco <sebacarrasco93@gmail.com>
2019-11-19 03:11:18 -0300

    Primeros CSS y jQuery implementado desde el CDN

---
+++ interaccion.js
@@ -0,0 +1,2 @@
+// Interacción con teclas en jQuery: https://stackoverflow.com/questions/1402698/binding-arrow-keys-in-js-jquery
+// Pregunta de seleccionar divs: https://stackoverflow.com/questions/15603617/choose-and-select-div-elements-with-keyboard-arrows-and-enter-keys
