b4b2ad90537b4beb47d24789051c7fbec9d724c0
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2019-11-19 07:19:12 -0300

    Agregado spatial navigation para simplificar el trabajo

--- interaccion.js
+++ interaccion.js
@@ -2,38 +2,50 @@
 // Pregunta de seleccionar divs: https://stackoverflow.com/questions/15603617/choose-and-select-div-elements-with-keyboard-arrows-and-enter-keys
 // Calendario hecho con jQuery: http://jsfiddle.net/eqrNT/
 
+$.getScript('https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js', function() {
+    $('.grupo')
+    .SpatialNavigation()
+    .focus(function() {
+        seleccionarGrupo(this)
+    })
+    .blur(function() { $(this).css('outline', ''); })
+    .first()
+    .focus();
+});
+
 function seleccionarGrupo() {
     grupos = $('.grupo')
     grupos.removeClass('elegido')
 
     grupoElegido = $('.grupo:focus')
     grupoElegido.addClass('elegido')
+    grupoElegido.css('outline', 'red')
 }
 
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 13:
-            console.log('enter')
-            seleccionarGrupo()
-        break;
+// $(document).keydown(function(e) {
+//     switch(e.which) {
+//         case 13:
+//             console.log('enter')
+//             seleccionarGrupo()
+//         break;
 
-        case 37:
-            console.log('izquierda')
-        break;
+//         case 37:
+//             console.log('izquierda')
+//         break;
 
-        case 38:
-            console.log('arriba')
-        break;
+//         case 38:
+//             console.log('arriba')
+//         break;
 
-        case 39:
-            console.log('derecha')
-        break;
+//         case 39:
+//             console.log('derecha')
+//         break;
 
-        case 40:
-            console.log('abajo')
-        break;
+//         case 40:
+//             console.log('abajo')
+//         break;
 
-        default: return;
-    }
-    e.preventDefault();
-});
+//         default: return;
+//     }
+//     e.preventDefault();
+// });
